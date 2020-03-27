d2efe22b1e7b17d9cc4e1100d0c814309837dec8
MegaKruk <filip.mazur.dev@gmail.com>
2020-03-27 01:04:52 +0100

    0 Rows, don't know why

--- pca.py
+++ pca.py
@@ -1,13 +1,8 @@
-import os, json
+import json
+import os
 import pandas as pd
-import numpy as np
-from sklearn.decomposition import PCA
 
-# todo: iterate over all folders, for now just load test folder because i'm impatient
-path_to_json = 'data/test'
-json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
-
-gtmetrix_data = pd.DataFrame(columns=['onload_time', 'first_contentful_paint_time', 'page_elements', 'report_url', 'redirect_duration', 'first_paint_time', 'dom_content_loaded_duration', 'dom_content_loaded_time', 'dom_interactive_time', 'page_bytes', 'page_load_time', 'html_bytes', 'fully_loaded_time', 'html_load_time', 'rum_speed_index', 'yslow_score', 'pagespeed_score', 'backend_duration', 'onload_duration', 'connect_duration'])
+gtmetrix_data = pd.DataFrame(columns=['onload_time', 'first_contentful_paint_time', 'page_elements', 'report_url', 'redirect_duration', 'first_paint_time', 'dom_content_loaded_duration', 'dom_content_loaded_time', 'dom_interactive_time', 'page_bytes', 'page_load_time', 'html_bytes', 'fully_loaded_time', 'html_load_time', 'rum_speed_index', 'yslow_score', 'pagespeed_score', 'backend_duration', 'onload_duration', 'connect_duration', 'date'])
 
 webtracker_data = pd.DataFrame(columns=['minify_total', 'responses_200', 'testStartOffset', 'bytesOut', 'gzip_savings', 'requestsFull',
                    'start_epoch', 'connections', 'bytesOutDoc', 'result', 'basePageSSLTime', 'docTime',
@@ -47,33 +42,45 @@ webtracker_data = pd.DataFrame(columns=['minify_total', 'responses_200', 'testSt
                    'FirstCPUIdle', 'run', 'step', 'effectiveBps', 'effectiveBpsDoc', 'domTime', 'aft', 'titleTime',
                    'domLoading', 'server_rtt', 'smallImageCount', 'bigImageCount', 'maybeCaptcha',
                    'heroElementTimes.Image', 'heroElementTimes.Heading', 'heroElementTimes.FirstPaintedHero',
-                   'heroElementTimes.LastPaintedHero', 'FirstPaintedHero', 'LastPaintedHero', 'avgRun', 'url', 'location'])
+                   'heroElementTimes.LastPaintedHero', 'FirstPaintedHero', 'LastPaintedHero', 'avgRun', 'url', 'location', 'date'])
 
-# todo: add columns - date from folder name and domain name from file name. gtmetrix_data has url in column 3
-# todo: solve problem - gtmetrix json has no information on test settings (we need location, page url, browser/mobile device)
-for index, js in enumerate(json_files):
-    with open(os.path.join(path_to_json, js)) as json_file:
-        json_text = json.load(json_file)
-        if 'data' in json_text:
-            # handle webtracker json
-            entry = json_text['data']['average']['firstView']
-            new_data = list()
-            for col in webtracker_data.columns:
-                if col == 'url':
-                    new_data.append(json_text['data']['url'])
-                elif col == 'location':
-                    new_data.append(json_text['data']['location'])
+
+path_to_json = 'data/'
+for subdir, dirs, files in os.walk(path_to_json):
+    date = subdir.replace(path_to_json, '')
+    print(date)
+    for file in files:
+        json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
+
+        # gtmetrix_data has url in column 3 natively
+        for index, js in enumerate(json_files):
+            with open(os.path.join(path_to_json, js)) as json_file:
+                json_text = json.load(json_file)
+                if 'data' in json_text:
+                    # handle webtracker json
+                    entry = json_text['data']['average']['firstView']
+                    new_data = list()
+                    for col in webtracker_data.columns:
+                        if col == 'url':
+                            new_data.append(json_text['data']['url'])
+                        elif col == 'location':
+                            new_data.append(json_text['data']['location'])
+                        elif col == 'date':
+                            new_data.append(date)
+                        else:
+                            new_data.append(entry[col])
+                    webtracker_data.loc[index] = new_data
+
                 else:
-                    new_data.append(entry[col])
-            webtracker_data.loc[index] = new_data
-
-        else:
-            # handle gtmetrix json
-            entry = json_text['results']
-            new_data = list()
-            for col in gtmetrix_data.columns:
-                new_data.append(entry[col])
-            gtmetrix_data.loc[index] = new_data
+                    # handle gtmetrix json
+                    entry = json_text['results']
+                    new_data = list()
+                    for col in gtmetrix_data.columns:
+                        if col == 'date':
+                            new_data.append(date)
+                        else:
+                            new_data.append(entry[col])
+                    gtmetrix_data.loc[index] = new_data
 
 print(gtmetrix_data)
 print(webtracker_data)
